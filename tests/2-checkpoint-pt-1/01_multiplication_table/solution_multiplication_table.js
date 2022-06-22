7ec9119ba703897e8a8db44cec3e79ecf4ae0f74
su-de-sh <sudesh7443@gmail.com>
2022-06-22 08:15:59 +0545

    final commit

--- tests/2-checkpoint-pt-1/01_multiplication_table/solution_multiplication_table.js
+++ tests/2-checkpoint-pt-1/01_multiplication_table/solution_multiplication_table.js
@@ -1,1 +1,12 @@
 /* eslint-disable no-unused-vars */
+function multiplicationTable(rows, columns) {
+  const matrix = [];
+  for (let i = 0; i < rows; i++) {
+    let intArr = [];
+    for (let j = 0; j < columns; j++) {
+      intArr.push((i + 1) * (j + 1));
+    }
+    matrix.push(intArr);
+  }
+  return matrix;
+}
