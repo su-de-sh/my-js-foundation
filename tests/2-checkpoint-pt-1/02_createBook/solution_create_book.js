614dcb6ca30394f7869f36bb45c40d79603ce1cf
su-de-sh <sudesh7443@gmail.com>
2022-06-22 08:58:35 +0545

    checkpoint create book problem solved

--- tests/2-checkpoint-pt-1/02_createBook/solution_create_book.js
+++ tests/2-checkpoint-pt-1/02_createBook/solution_create_book.js
@@ -1,1 +1,39 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
+function createBook(id, title, author, price, rating = []) {
+  const obj = {
+    id,
+    title,
+    author,
+    price,
+    rating: [],
+    getInfo() {
+      return `${title} by ${author}`;
+    },
+    getPrice() {
+      return this.price;
+    },
+    addRating(stars) {
+      this.rating.push(stars);
+    },
+    getRating() {
+      console.log(rating);
+      return (
+        this.rating
+          .map((n) => {
+            return n.length;
+          })
+          .reduce((a, b) => {
+            return a + b;
+          }) / this.rating.length
+      );
+    },
+  };
+  const obj1 = Object.create(obj);
+
+  obj1.id = id;
+  obj1.title = title;
+  obj1.author = author;
+  obj1.rating = rating;
+  obj1.price = price;
+  return obj1;
+}
