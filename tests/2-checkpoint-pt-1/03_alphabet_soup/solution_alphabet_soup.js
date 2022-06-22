fd3f35e6371eb91a4a1da46f94821fd564a06cc5
su-de-sh <sudesh7443@gmail.com>
2022-06-22 09:19:45 +0545

    checkpoint alphabet problem solved

--- tests/2-checkpoint-pt-1/03_alphabet_soup/solution_alphabet_soup.js
+++ tests/2-checkpoint-pt-1/03_alphabet_soup/solution_alphabet_soup.js
@@ -1,1 +1,10 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
+const soup = (phrase, bank) => {
+  if (bank.length < phrase.length) return false;
+  bank = bank.split("");
+
+  let fileterdPhrase = phrase.split("").filter((n, i) => {
+    return bank.includes(n);
+  });
+  return fileterdPhrase.length === phrase.length ? true : false;
+};
