c879df9d30684ce91dc00a9cc4011bf436d022ad
su-de-sh <sudesh7443@gmail.com>
2022-06-22 08:01:21 +0545

    findObj solved

--- tests/2-checkpoint-pt-1/00_find_obj_properties/solution_find_obj_properties.js
+++ tests/2-checkpoint-pt-1/00_find_obj_properties/solution_find_obj_properties.js
@@ -1,5 +1,19 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
-function findObjPropsHasOwn() {
-  let keys = "";
-  return keys;
+function findObjPropsHasOwn(obj) {
+  let myStr = [];
+  for (let keys in obj) {
+    if (obj.hasOwnProperty(keys)) {
+      myStr.push(keys);
+    }
+  }
+  return myStr.join(", ");
+}
+
+function findObjKeys(obj) {
+  let myStr = [];
+  Object.keys(obj).forEach((n) => {
+    myStr.push(n);
+  });
+
+  return myStr.join(", ");
 }
