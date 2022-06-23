72f421fb3c8d9ff78a47e280e6bdaf5ac94510ad
su-de-sh <sudesh7443@gmail.com>
2022-06-23 21:29:46 +0545

    create obj with closure problem solved

--- tests/3-testfirst-part-2/06-functions-and-closure/functions.js
+++ tests/3-testfirst-part-2/06-functions-and-closure/functions.js
@@ -17,3 +17,47 @@ function yourFunctionRunner() {
   });
   return myStr;
 }
+
+//makeAdder
+
+function makeAdder(num) {
+  function innerAdder(num1) {
+    return num + num1;
+  }
+
+  return innerAdder;
+}
+
+//Once
+
+function once(func) {
+  let count = 0;
+  function anotherFunc() {
+    count++;
+    if (count > 1) return "the function has already been called...";
+    else return func();
+  }
+
+  return anotherFunc;
+}
+
+function createObjectWithClosures() {
+  let number = 0;
+  const obj = {
+    oneIncrementer() {
+      number += 1;
+    },
+    tensIncrementer() {
+      number += 10;
+    },
+    getValue() {
+      return number;
+    },
+    setValue(num = 0) {
+      number = num;
+      return number;
+    },
+  };
+
+  return obj;
+}
