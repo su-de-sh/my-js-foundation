5fea47ab6c0c631e2a645c4cd965d4f80dc7c021
su-de-sh <sudesh7443@gmail.com>
2022-06-23 12:21:27 +0545

    completed upto yourFunctionRunner

--- tests/3-testfirst-part-2/06-functions-and-closure/functions.js
+++ tests/3-testfirst-part-2/06-functions-and-closure/functions.js
@@ -1,1 +1,19 @@
 /* eslint-disable no-unused-vars */
+
+function concatString() {
+  let myStr = "";
+  array = Array.from(arguments);
+  array.forEach((element) => {
+    myStr += element;
+  });
+  return myStr;
+}
+
+function yourFunctionRunner() {
+  let myStr = "";
+  let func = Array.from(arguments);
+  func.forEach((element) => {
+    myStr += element();
+  });
+  return myStr;
+}
