6050b41c95758267d5239b304d246c219820fdcb
su-de-sh <sudesh7443@gmail.com>
2022-06-24 09:04:14 +0545

    completed upto filter

--- tests/3-testfirst-part-2/07-functional-programming/00-recreate-array-methods/functional.js
+++ tests/3-testfirst-part-2/07-functional-programming/00-recreate-array-methods/functional.js
@@ -15,3 +15,13 @@ map = (array, callback) => {
   }
   return newArr;
 };
+
+// filter
+
+filter = (array, callback) => {
+  const newArr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) newArr.push(array[i]);
+  }
+  return newArr;
+};
