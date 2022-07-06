3d1edea82bdafed97914691908589aa51cb9ea23
su-de-sh <sudesh7443@gmail.com>
2022-07-06 08:46:28 +0545

    reject function done

--- tests/5-checkpoint-pt-2/01_reject/solution_reject.js
+++ tests/5-checkpoint-pt-2/01_reject/solution_reject.js
@@ -1,1 +1,8 @@
 /* eslint-disable no-unused-vars */
+function reject(arrayOfNumbers, isNegNum) {
+  return arrayOfNumbers.filter((element) => {
+    if (!isNegNum(element)) {
+      return element;
+    }
+  });
+}
