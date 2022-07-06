b24f83caee574faea2d759f31b2506f679628179
su-de-sh <sudesh7443@gmail.com>
2022-07-06 10:47:26 +0545

    03- thrice done

--- tests/5-checkpoint-pt-2/03_thrice/solution_thrice.js
+++ tests/5-checkpoint-pt-2/03_thrice/solution_thrice.js
@@ -1,1 +1,12 @@
 /* eslint-disable no-unused-vars */
+function thrice(callback) {
+  let count = 0;
+  function callback1() {
+    count++;
+    if (count > 3) return undefined;
+    else {
+      return callback();
+    }
+  }
+  return callback1;
+}
