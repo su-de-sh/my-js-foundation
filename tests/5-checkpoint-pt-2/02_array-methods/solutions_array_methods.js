3a2029e9c656344dd52ffb7bfd9d0433b4442600
su-de-sh <sudesh7443@gmail.com>
2022-07-06 09:00:39 +0545

    checkpoints-2 02array-method done

--- tests/5-checkpoint-pt-2/02_array-methods/solutions_array_methods.js
+++ tests/5-checkpoint-pt-2/02_array-methods/solutions_array_methods.js
@@ -1,1 +1,31 @@
 /* eslint-disable no-unused-vars */
+function useMapToUpperCase(str) {
+  return str.split(" ").map((element) => {
+    return element.toUpperCase();
+  });
+}
+
+function useFilter(array) {
+  return array.filter((element) => {
+    return element.match("@");
+  });
+}
+
+function sumWithReduce(array, startingVal = 0) {
+  return array.reduce((a, b) => {
+    return a + b;
+  }, startingVal);
+}
+
+function filterEvensDoubleAndSum(array) {
+  return array
+    .filter((element) => {
+      return element % 2 === 0;
+    })
+    .map((element) => {
+      return element * 2;
+    })
+    .reduce((a, b) => {
+      return a + b;
+    });
+}
