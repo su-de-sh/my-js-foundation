80e221fe828556441de793db50752ba7028a9108
su-de-sh <sudesh7443@gmail.com>
2022-07-06 13:25:33 +0545

    RPNCalculator function done

--- tests/6-testfirst-part-3/08-Classes/00-refactor-RPN-Calculator/solution-refactor-rpn-calculator.js
+++ tests/6-testfirst-part-3/08-Classes/00-refactor-RPN-Calculator/solution-refactor-rpn-calculator.js
@@ -3,3 +3,58 @@
 /*
   Refactor the constructor function below and use ES2015 Class Syntax
 */
+
+class RPNCalculator {
+  constructor() {
+    this.array = [];
+  }
+
+  push = function (val) {
+    this.array.push(val);
+  };
+}
+
+RPNCalculator.prototype.plus = function () {
+  if (!this.array.length) throw "rpnCalculatorInstance is empty";
+  this.array.push(
+    this.array.reduce((a, b) => {
+      return a + b;
+    })
+  );
+  console.log(this.array);
+};
+
+RPNCalculator.prototype.minus = function () {
+  if (!this.array.length) throw "rpnCalculatorInstance is empty";
+  this.array.push(
+    this.array.reduce((a, b) => {
+      return a - b;
+    })
+  );
+  console.log(this.array);
+};
+
+RPNCalculator.prototype.times = function () {
+  if (!this.array.length) throw "rpnCalculatorInstance is empty";
+  this.array.push(
+    this.array.reduce((a, b) => {
+      return a * b;
+    })
+  );
+  console.log(this.array);
+};
+
+RPNCalculator.prototype.divide = function () {
+  if (!this.array.length) throw "rpnCalculatorInstance is empty";
+  this.array.push(
+    this.array.reduce((a, b) => {
+      return a / b;
+    })
+  );
+  console.log(this.array);
+};
+
+RPNCalculator.prototype.value = function () {
+  this.array = [this.array[this.array.length - 1]];
+  return this.array[0];
+};
