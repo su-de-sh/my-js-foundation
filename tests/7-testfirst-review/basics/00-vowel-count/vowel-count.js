98af3deb1edfd67f015bf5f48b73b10792dd78a3
su-de-sh <sudesh7443@gmail.com>
2022-07-06 21:49:41 +0545

    vowel problem solved

--- tests/7-testfirst-review/basics/00-vowel-count/vowel-count.js
+++ tests/7-testfirst-review/basics/00-vowel-count/vowel-count.js
@@ -1,1 +1,18 @@
 /* eslint-disable no-unused-vars */
+
+function vowelsCount(string) {
+  const map = { a: 0, e: 0, i: 0, o: 0, u: 0, total: 0 };
+  if (!string) return map;
+  return string
+    .toLowerCase()
+    .split("")
+    .reduce((a, b) => {
+      if (b === "a") a.a++, a.total++;
+      if (b === "e") a.e++, a.total++;
+      if (b === "i") a.i++, a.total++;
+      if (b === "o") a.o++, a.total++;
+      if (b === "u") a.u++, a.total++;
+
+      return a;
+    }, map);
+}
