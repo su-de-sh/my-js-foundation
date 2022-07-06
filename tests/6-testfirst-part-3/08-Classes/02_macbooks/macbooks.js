488dc5ac866fe6ac5192de3884745b4b8d813e03
su-de-sh <sudesh7443@gmail.com>
2022-07-06 18:01:52 +0545

    02 macbooks done

--- tests/6-testfirst-part-3/08-Classes/02_macbooks/macbooks.js
+++ tests/6-testfirst-part-3/08-Classes/02_macbooks/macbooks.js
@@ -1,1 +1,16 @@
 /* eslint-disable no-unused-vars */
+function Laptop(year, size) {
+  this.year = year;
+  this.hd = size;
+}
+
+Laptop.prototype.checkSpecs = function () {
+  return `Year: ${this.year}, HD: ${this.hd}`;
+};
+
+class Macbook extends Laptop {
+  constructor(year, size, color) {
+    super(year, size);
+    this.color = color;
+  }
+}
