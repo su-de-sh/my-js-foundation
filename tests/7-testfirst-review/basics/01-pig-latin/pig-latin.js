eef1a8b01de511c282c9990d860513ca25c8d6cd
su-de-sh <sudesh7443@gmail.com>
2022-07-07 11:03:07 +0545

    pig latin done

--- tests/7-testfirst-review/basics/01-pig-latin/pig-latin.js
+++ tests/7-testfirst-review/basics/01-pig-latin/pig-latin.js
@@ -1,1 +1,26 @@
 /* eslint-disable no-unused-vars */
+function pigify(mystr) {
+  const vowel = "aeiou";
+  const myStrArray = [];
+  debugger;
+  mystr.split(" ").forEach((str) => {
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === "q" && str[i + 1] === "u") {
+        i++;
+        continue;
+      }
+      if (vowel.includes(str[i])) {
+        myStrArray.push(
+          str.slice(str.indexOf(str[i]), str.length) +
+            str.slice(0, str.indexOf(str[i])) +
+            "ay"
+        );
+        break;
+      }
+    }
+  });
+
+  return myStrArray.join(" ");
+}
+
+pigify("apple");
