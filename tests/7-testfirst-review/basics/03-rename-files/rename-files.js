99d87c954a884726ed79faf55e2ad6fc33b329f4
su-de-sh <sudesh7443@gmail.com>
2022-07-07 13:55:31 +0545

    renamefiles done

--- tests/7-testfirst-review/basics/03-rename-files/rename-files.js
+++ tests/7-testfirst-review/basics/03-rename-files/rename-files.js
@@ -1,1 +1,23 @@
 /* eslint-disable no-unused-vars */
+function renameFiles(fileNames) {
+  let myHash = {};
+  let n = 1;
+  let updatedFileNames = [];
+
+  fileNames.forEach((file) => {
+    if (myHash[file]) {
+      myHash[file]++;
+
+      let filename = `${file}(${n})`;
+      while (updatedFileNames.includes(filename)) {
+        n++;
+        filename = `${file}(${n})`;
+      }
+      updatedFileNames.push(filename);
+    } else {
+      myHash[file] = 1;
+      updatedFileNames.push(file);
+    }
+  });
+  return updatedFileNames;
+}
