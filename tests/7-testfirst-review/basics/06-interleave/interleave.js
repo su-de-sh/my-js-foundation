9a5c03ce6e7a79ab20c137f5ac441f8d18ae6d6a
su-de-sh <sudesh7443@gmail.com>
2022-07-08 23:59:28 +0545

    fixed the variable name

--- tests/7-testfirst-review/basics/06-interleave/interleave.js
+++ tests/7-testfirst-review/basics/06-interleave/interleave.js
@@ -2,8 +2,8 @@ function interleave() {
   let count = 0;
   let args = Array.from(arguments);
   let myStr = "";
-  let max = maxChar(args);
-  for (let i = 0; i < max; i++) {
+  let longestStringLength = maxChar(args);
+  for (let i = 0; i < longestStringLength; i++) {
     let currStr = args[i];
     for (let j = 0; j < args.length; j++) {
       let currStr = args[j][i];
