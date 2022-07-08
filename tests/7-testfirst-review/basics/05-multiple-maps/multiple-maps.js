1fe39ea43aef80ac5e6d96e12b2e54a8b795c359
su-de-sh <sudesh7443@gmail.com>
2022-07-08 12:44:17 +0545

    multiple maps done

--- tests/7-testfirst-review/basics/05-multiple-maps/multiple-maps.js
+++ tests/7-testfirst-review/basics/05-multiple-maps/multiple-maps.js
@@ -1,1 +1,12 @@
 /* eslint-disable no-unused-vars */
+
+function multipleMaps(array, mapArray) {
+  return array.map((element) => {
+    for (let i of mapArray) {
+      if (i[element] !== undefined) {
+        return i[element];
+        break;
+      }
+    }
+  });
+}
